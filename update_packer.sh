b41c3b423bb83c5911eefe7ce5fe5c1dcac56a79
Thecode42 <devsoft25@gmail.com>
2025-02-22 08:22:23 -0500

    Fix artifact path in Packer configuration and update script for Tomcat deployment

--- update_packer.sh
+++ update_packer.sh
@@ -2,6 +2,6 @@
 
 ARTIFACT_NAME=$(ls target/*.war | head -n 1)
 BASENAME=$(basename "$ARTIFACT_NAME")
-sed -i "s|/target/artefacto.war|/target/$BASENAME|" packer.json
+sed -i '' "s|artefacto.war|$BASENAME|" packer.json
 
 echo "Archivo packer.json actualizado con el artefacto: $BASENAME"
