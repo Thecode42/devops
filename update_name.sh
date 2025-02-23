46910d36802f2b8474215fce994dbb0bdefa43e5
Thecode42 <devsoft25@gmail.com>
2025-02-23 09:04:59 -0500

    Update update_name script to dynamically set artifact name in Dockerfile

--- update_name.sh
+++ update_name.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ARTIFACT_NAME=$(ls target/*.war | head -n 1)
-BASENAME="devops.war"
+BASENAME=$(basename "$ARTIFACT_NAME")
 sed -i '' "s|artefacto.war|$BASENAME|" Dockerfile
 
-echo "Archivo packer.json actualizado con el artefacto: $BASENAME"
+echo "Archivo Dockerfile actualizado con el artefacto: $BASENAME"
